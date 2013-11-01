00ce459235c490af6e19e1527a46ba648632e04d
cheinrich09 <cah2792@IMM-27.main.ad.rit.edu>
2013-11-01 15:26:35 -0400

    The Initial Starting Point for the project

---
+++ Trailed/Assets/Scripts/GrabScript.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GrabScript : MonoBehaviour {
+	
+	public GameObject[] players;
+	public int throwingForce = 50;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//if(holdingBall)
+		//{
+			if(Input.GetKeyDown(KeyCode.F))
+			{
+				
+				//heldBall.rigidbody.velocity = transform.TransformDirection(new Vector3(0, 5, throwingForce));
+				//holdingBall = false;
+				//heldBall.GetComponent<BallScript>().held = false;
+				//heldBall.GetComponent<BallScript>().ballActive = true;
+				//heldBall = null;
+			}
+		//}
+		//else
+		//{
+			if(Input.GetKeyDown(KeyCode.E))
+			{
+				for(int i = 0; i < players.Length; i++)
+				{
+					if(Vector3.Distance(transform.position, players[i].transform.position) < 10)
+					{
+						Debug.Log("Caught");
+						break;
+					}
+				}
+			}
+		//}
+	}
+}
---
+++ Trailed/Assets/Scripts/TrailScript.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrailScript : MonoBehaviour {
+	float trailTimer = 0;
+	float maxTime = .5f;
+	int maxPoints = 20;
+	Vector3[] positions;
+	int numPoints = 0;
+	LineRenderer trail;
+	// Use this for initialization
+	void Start () {
+		trail = GetComponent<LineRenderer>();
+		positions = new Vector3[maxPoints];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		trailTimer += Time.deltaTime;
+		Debug.Log(trailTimer);
+		Vector3 playerPos = trail.gameObject.transform.position;
+		if(trailTimer >= maxTime)
+		{
+			trailTimer = 0;
+			Vector3 newPos = new Vector3(playerPos.x, playerPos.y, playerPos.z);
+			if(numPoints < maxPoints)
+			{
+				Debug.Log("Point Added");
+				trail.SetVertexCount(numPoints + 1);
+				trail.SetPosition(numPoints, newPos);
+				positions[numPoints] = newPos;
+				numPoints++;
+			}
+			else
+			{
+				Vector3[] temp = new Vector3[numPoints];
+				for(int i = 0; i < numPoints - 1; i++)
+				{
+					positions[i] = positions[i + 1];
+					
+				}
+				
+				positions[numPoints - 1] = newPos;
+				
+				for(int i = 0; i < numPoints; i++)
+				{
+					trail.SetPosition(i, positions[i]);
+				}
+			}
+		}
+		Debug.Log ("X: " + playerPos.x);
+		Debug.Log ("Y: " + playerPos.y);
+		Debug.Log ("Z: " + playerPos.z);
+		
+	}
+}
